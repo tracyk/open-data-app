d12cf9b317ad1236ebc01fb174830d5b0cd06cd2
Tracy <tracykarnabi@hotmail.com>
2012-04-24 19:09:50 -0400

    added doc block to edit file

--- admin/edit.php
+++ admin/edit.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+	*Ottawa Mapped
+	*Displays the list and map for the Open Data Set
+	*
+	*@package ca.tracyk.opp-data-app
+	*@copyright 2012 Tracy K. Designs
+	*@author Tracy Karnabi <tracy@tracykarnabi.com>
+	*@link https://github.com/tracyk/open-data-app
+	*license New BSD License 
+ */
+
 $errors = array();
 
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
