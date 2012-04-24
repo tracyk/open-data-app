9318af3016e68f9dd96c557b25467b6a08385482
Tracy <tracykarnabi@hotmail.com>
2012-04-24 18:49:58 -0400

    added rating system

---
+++ rate.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+	*Ottawa Mapped
+	*Displays the list and map for the Open Data Set
+	*
+	*@package ca.tracyk.opp-data-app
+	*@copyright 2012 Tracy K. Designs
+	*@author Tracy Karnabi <tracy@tracykarnabi.com>
+	*@link https://github.com/tracyk/open-data-app
+	*license New BSD License 
+ */
+
+include 'includes/theme-top.php';
+
+?>
+
+<div class="exemple">
+	<div class="basic" id="12_1"></div>
+</div>
+
+
+<?php
+
+include 'includes/theme-bottom.php';
+
+?>
+
