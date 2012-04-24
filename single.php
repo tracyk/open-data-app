b1cc1c2129bceb805ac551dc0a270d88a3d98b07
Tracy <tracykarnabi@hotmail.com>
2012-04-24 18:48:31 -0400

    added doc block to single page. added theme top and bottom
    styling

--- single.php
+++ single.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+	*Ottawa Mapped
+	*Displays the list and map for the Open Data Set
+	*
+	*@package ca.tracyk.opp-data-app
+	*@copyright 2012 Tracy K. Designs
+	*@author Tracy Karnabi <tracy@tracykarnabi.com>
+	*@link https://github.com/tracyk/open-data-app
+	*license New BSD License 
+ */
+	
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 if (empty($id)) {
@@ -7,62 +18,49 @@ if (empty($id)) {
 	exit;
 }
 
-// Only connect to the database if there is an ID, becuse this is after the above if-statement
-// Without an ID there is no point connecting to the database
 require_once 'includes/db.php';
 
-// ->prepare() allows us to execute SQL with user input
 $sql = $db->prepare('
-	SELECT id, name,longitude,latitude,street_address
+	SELECT id, name, latitude, longitude, address
 	FROM ottawamapped
 	WHERE id = :id
 ');
 
-// ->bindValue() lets us fill in placeholders in our prepared statement
-// :id is a placeholder for us to SECURELY put information from the user
 $sql->bindValue(':id', $id, PDO::PARAM_INT);
-
-// Performs the SQL query on the database
 $sql->execute();
-
-// Gets the results from the SQL query and stores them in a variable
-// ->fetch() gets a single result
-// ->fetchAll() gets all the possible results
 $results = $sql->fetch();
 
-// Redirect the user back to the homepage if there are no database results
-// No results will happen if they change the ?id=4 to include an ID that doesn't exist
 if (empty($results)) {
 	header('Location: index.php');
-	exit; // Stop the PHP compiler right here and immediately redirect the user
+	exit;
 }
 
-?><!DOCTYPE HTML>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title><?php echo $results['name']; ?> &middot; Ottawa Mapped</title>
-</head>
-<body>
-	
-	<h1><?php echo $results['name']; ?></h1>
-	<p>Longitude: <?php echo $results['longitude']; ?></p>
-	<p>Latitude: <?php echo $results['latitude']; ?></p>
-	<p>Street Address: <?php echo $results['address']; ?></p>
-	
-	
-</body>
-</html>
+$title = $results['name'];
+
+include 'includes/theme-top.php';
+
+?>
+
+	<div id="container">
+		<div class="single-page">
+			<h1><?php echo $results['name']; ?></h1>
+			<dl>
+				<dt>Address</dt><dd><?php echo $results['address']; ?></dd>
+				<dt>Longitude</dt><dd><?php echo $results['longitude']; ?></dd>
+				<dt>Latitude</dt><dd><?php echo $results['latitude']; ?></dd>
+			</dl>
+			
+			<div class="exemple">
+				<div class="basic" id="12_1"></div>
+			</div>
+		</div>	
+		<a href="index.php" class="admin home-class">Home</a>
+		<a href="admin/index.php" class="admin admin-class-button">Admin</a>
+	</div>
 
 
+<?php
 
+include 'includes/theme-bottom.php';
 
-
-
-
-
-
-
-
-
-
+?>
