962487e231c22227df6db53635e3e9f67da321c7
Tracy <tracykarnabi@hotmail.com>
2012-04-24 18:54:39 -0400

    created doc block, created container and clear fix for body
    Container will allow to better style in control css for index

--- index.php
+++ index.php
@@ -1,52 +1,50 @@
 <?php
+
 /**
+	*Ottawa Mapped
 	*Displays the list and map for the Open Data Set
 	*
 	*@package ca.tracyk.opp-data-app
 	*@copyright 2012 Tracy K. Designs
 	*@author Tracy Karnabi <tracy@tracykarnabi.com>
 	*@link https://github.com/tracyk/open-data-app
-	*license New BSD License <add readme link from github>
-	*@version 1.0.0
+	*license New BSD License 
  */
-require_once 'includes/filter-wrapper.php';
+ 
+require_once 'includes/db.php';
 
-// `->exec()` allows us to perform SQL and NOT expect results
-// `->query()` allows us to perform SQL and expect results
-require_once 'includes/db.php'; 
 $results = $db->query('
-	SELECT id,name,latitude,longitude,address
+	SELECT id, name, latitude, longitude, address
 	FROM ottawamapped
 	ORDER BY name ASC
 ');
-?><!DOCTYPE HTML>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Ottawa Mapped</title>
-</head>
-<body>
 
-<a href="add.php"> Add a Volleyball Court</a>
-	
-	<ul>
-	<?php
-	/*
-		foreach ($results as $ottawamapped) {
-			echo '<li>' . $ottawamapped['name'] . '</li>';
-		}
-	*/
-	?>
-	
-	<?php foreach ($results as $ottawamapped) : ?>
-		<li>
-			<a href="single.php?id=<?php echo $ottawamapped['id']; ?>"><?php echo $ottawamapped['name']; ?></a>
-			&bull;
-			<a href="edit.php?id=<?php echo $ottawamapped['id']; ?>">Edit</a>
-            <a href="delete.php?id=<?php echo $ottawamapped['id']; ?>">Delete</a>
+include 'includes/theme-top.php';
+
+?>
+
+<div id="container" class="clearfix">
+	<ol class="mapped">
+	<?php foreach ($results as $volley) : ?>
+		<li itemscope itemtype="http://schema.org/TouristAttraction">
+			<a href="single.php?id=<?php echo $volley['id']; ?>" itemprop="name"><?php echo $volley['name']; ?></a>
+			<span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
+				<meta itemprop="latitude" content="<?php echo $volley['latitude']; ?>">
+				<meta itemprop="longitude" content="<?php echo $volley['longitude']; ?>">
+			</span>
 		</li>
 	<?php endforeach; ?>
-	</ul>
+	</ol>
 	
-</body>
-</html>
+	
+		<a href="admin/index.php" class="admin">Admin Login</a>
+		<div id="map"></div>
+	
+</div>
+
+<?php
+
+include 'includes/theme-bottom.php';
+
+?>
+
