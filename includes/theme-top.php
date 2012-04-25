a90f6b989db9ea104ad06d21c9091e1b728995b7
Tracy <tracykarnabi@hotmail.com>
2012-04-25 13:42:49 -0400

    added correct path to css in theme-top
    this normally wouldn't work but does in phpfog

--- includes/theme-top.php
+++ includes/theme-top.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset=utf-8>
 	<title><?php if (isset($title)) { echo $title . ' · '; } ?>Volleyball Courts</title>
-	<link href="css/volleyball.css" rel="stylesheet">
+	<link href="../css/volleyball.css" rel="stylesheet">
 	<link rel="stylesheet" href="jquery/jRating.jquery.css" type="text/css" />
 	<script src="js/modernizr.dev.js"></script>
 	<link href='http://fonts.googleapis.com/css?family=Averia+Libre' rel='stylesheet' type='text/css'>
@@ -12,6 +12,6 @@
 <div class="wrapper">
 
 <header> 
-	<img src="images/ottawa-mapped-banner.jpg"  alt="main banner in the document"> 
+	<img src="../images/ottawa-mapped-banner.jpg"  alt="main banner in the document"> 
 </header>
 
